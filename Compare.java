03bb5cc4201d78ba1b4a27263a941890d5cfcd36
sachida1997 <125948307+sachida1997@users.noreply.github.com>
2023-11-26 20:33:53 +0530

    Add files via upload

---
+++ Compare.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Compare 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("enter the  1st String");
+    Scanner sc=new Scanner(System.in);
+    String str=sc.nextLine();
+String str1=sc.nextLine();
+boolean result=str.equals(str1);
+System.out.println(result);
+	}
+}
